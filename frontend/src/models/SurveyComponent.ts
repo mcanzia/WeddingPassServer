8dce04d75e6b185f807568aaad8c2632b7c2f8e6
mcanziani <canziani.developer@gmail.com>
2024-11-09 18:56:14 -0500

    fixing SurveyComponent error

--- frontend/src/models/SurveyComponent.ts
+++ frontend/src/models/SurveyComponent.ts
@@ -4,11 +4,11 @@ export class SurveyComponent {
     label?: string;
     type: string;
     friendlyName: string;
-    order: number;
+    order?: number;
     value?: any;
     options?: Array<string>;
 
-    constructor(type: string, friendlyName: string, order: number, id?: string, options?: Array<string>, label?: string, value?: any) {
+    constructor(type: string, friendlyName: string, order?: number, id?: string, options?: Array<string>, label?: string, value?: any) {
         this.id = id;
         this.label = label;
         this.type = type;
