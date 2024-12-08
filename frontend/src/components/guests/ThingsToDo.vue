44fc39952c455785044ddb3ed3e50f06731c2083
Michael Canziani <canziani.michael@gmail.com>
2024-12-08 18:50:02 -0500

    added favicon, made Q&A and things to do pages pretty. Just need to do some image uploads tomorrow

--- frontend/src/components/guests/ThingsToDo.vue
+++ frontend/src/components/guests/ThingsToDo.vue
@@ -1,37 +1,45 @@
 <template>
-    <div>
-    <p>There are a lot of great places to visit in Udaipur, but here are some of our favorites!</p>
 
-    <h2>City Palace</h2>
-    📍Old City, Udaipur, Rajasthan 313001, India
-    <p> Visit the actual palace during the daytime to take in the beautiful artwork and architecture of the City Palace! Built over 400 years, the palace is a labyrinth of courtyards, pavilions, terraces, corridors, rooms, and hanging gardens, each with a distinctive design. Notable sections include the Mor Chowk, known for its exquisite peacock mosaics, the Sheesh Mahal (Palace of Mirrors), and the Amar Vilas, the highest point of the palace with a breathtaking garden. The complex houses several museums displaying royal artifacts, paintings, and traditional Rajasthani attire. The intricate carvings, mirror-work, murals, and colored glass enhance its beauty, making it a testament to the artistic prowess of the Rajput era. Find more information here: citypalacemuseum.org </p>
+<Card class="w-[90vw] justify-self-center bg-[#fff7e7]">
+    <CardHeader class="flex flex-col items-center rounded-lg justify-self-center bg-[#fff7e7] w-[90vw] font-['Faculty_Glyphic']">
+        <div class="m-4 space-y-1 text-base">
+            <p>There are a lot of great places to visit in Udaipur, but here are some of our favorites!</p>
+        </div>
+        <div class="m-4 space-y-1">
+            <p class="font-bold text-lg">City Palace</p>
+            <p class="text-base font-bold"> 📍Old City, Udaipur, Rajasthan 313001, India </p>
+            <p class="text-base">Visit the actual palace during the daytime to take in the beautiful artwork and architecture of the City Palace! Built over 400 years, the palace is a labyrinth of courtyards, pavilions, terraces, corridors, rooms, and hanging gardens, each with a distinctive design. Notable sections include the Mor Chowk, known for its exquisite peacock mosaics, the Sheesh Mahal (Palace of Mirrors), and the Amar Vilas, the highest point of the palace with a breathtaking garden. The complex houses several museums displaying royal artifacts, paintings, and traditional Rajasthani attire. The intricate carvings, mirror-work, murals, and colored glass enhance its beauty, making it a testament to the artistic prowess of the Rajput era. Find more information here: citypalacemuseum.org </p>
+            <p class="text-base font-style: italic">Entry fee into the museum is ₹400 (rupees) for adults and ₹150 (rupees) for children 5-18 years old.</p>
+        </div>
+  
+        <div class="m-4 space-y-1">
+            <p class="font-bold text-lg">Bagore Ki Haveli</p>
+            <p class="text-base font-bold"> 📍Haveli, opp. Gangaur Ghat Marg, near Hotel, Hotel Gangaur Palace, Pichola, Udaipur, Rajasthan 313001, India </p>
+            <p class="text-base">Visit a restored mansion and watch a traditional dance performance! Bagore Ki Haveli is a haveli that stands magnificently on the waterfront of the surreal Lake Pichola at Gangori Ghat. Builtin the 18th century by Amar Chand Badwa, the then Prime minister of Mewar kingdom, Bagore Ki Haveli has been restored and converted into a museum. Shri Amarchand Badwa, who served as the prime minister of the Mewar kingdom from 1751 to 1778, built Bagore Ki Haveli. Following his death, the haveli came under the possession of the Mewar royal family. In 1878, Maharaj Shakti Singh of Bagore further extended the haveli by building the triple-arched gateway, and since then, it has been known as Bagore ki Haveli. Till 1947, the haveli remained in the possession of Mewar state. Post-India’s Independence, the Rajasthan government used the Haveli to house government employees. Find more information here: udaipurtourism.co.in/bagore-ki-haveli-udaipur </p>
+            <p class="font-style: italic"> Haveli: For non-Indians, the entry fee into the museum is ₹60(rupees) for adults and ₹30 (rupees) for children 5-12 years old. For Indian citizens and OCI Card holders, the entry fee into the museum is ₹100 (rupees) for adults and ₹50 (rupees) for children 5-12 years old. There is an additional charge of ₹50 (rupees) for each camera.</p>
+            <p class="font-style: italic"> Dharodar Dance Show: For non-Indians, the show is ₹150(rupees) for adults and ₹75 (rupees) for children 5-12 years old. For Indian citizens and OCI Card holders, the entry fee into the museum is ₹90 (rupees) for adults and ₹45 (rupees) for children 5-12 years old. There is an additional charge of ₹150 (rupees) for each camera.</p>
+        </div>
 
-    <i>Entry fee into the museum is ₹400 (rupees) for adults and ₹150 (rupees) for children 5-18 years old.</i>
+        <div class="m-4 space-y-1">
+            <p class="font-bold text-lg">Shilpgram</p>
+            <p class="text-base font-bold">📍Shilpgram, Udaipur, Bari, Rajasthan 313001, India</p>
+            <p class="text-base">Shilpgram is a vibrant ethnographic museum and traditional crafts village spread over 70 acres. Established to promote and preserve the art, craft, and cultural heritage of rural India, particularly the Western states, it serves as a living museum depicting the lifestyles of the folk and tribal people of the region. The complex includes huts and structures representing different geographical and ethnic groups, providing insights into their traditional architecture, arts, and crafts. Shilpgram hosts an annual crafts fair at the end of December, attracting artisans and performers from across India, making it a hub for cultural exchange and a celebration of Indias rich rural arts and crafts.Find more information here: www.shilpgram.in/Home/Shilpgram-Udaipur
+            </p>
+            <p class="text-base font-style: italic">Entry fee is ₹50 (rupees) for non-Indians and ₹30 (rupees) for Indian citizens and OCI card holders.</p>
+        </div>
 
-    <h3>Bagore Ki Haveli</h3>
-    📍Haveli, opp. Gangaur Ghat Marg, near Hotel, Hotel Gangaur Palace, Pichola, Udaipur, Rajasthan 313001, India
+        <div class="m-4 space-y-1">
+            <p class="font-bold text-lg">Vintage Car Museum at Fateh Vilas by Fateh Collection</p>
+            <p class="text-base font-bold">📍Bhujra, Sisarma Road, Old City, Brahmpuri, Udaipur, Rajasthan 313001, India</p>
+            <p class="text-base">A must-visit for car lovers! The Vintage and Classic Car Museum is a car museum with some of the rarest classic and vintage car collections. This museum is owned by Arvind Singh Mewar, the king of Mewar (where Udaipur is). The museum also houses some famous cars, including the 1934 Rolls Royce used by James Bond in Octopussy! Find more information here: www.hrhhotels.com/vintage-classic-car-collection.php</p>
+            <p class="text-base font-style: italic">Entry fee into the museum is ₹400 (rupees) for adults and ₹250 (rupees) for children.</p>
+        </div>
+      <img src="/images/engage.jpg" alt="Guest Home Picture" class="mt-3 w-auto h-auto max-w-xs object-contain" />
+    </CardHeader>
+    <CardContent>
 
-    <p>Visit a restored mansion and watch a traditional dance performance! Bagore Ki Haveli is a haveli that stands magnificently on the waterfront of the surreal Lake Pichola at Gangori Ghat. Builtin the 18th century by Amar Chand Badwa, the then Prime minister of Mewar kingdom, Bagore Ki Haveli has been restored and converted into a museum. Shri Amarchand Badwa, who served as the prime minister of the Mewar kingdom from 1751 to 1778, built Bagore Ki Haveli. Following his death, the haveli came under the possession of the Mewar royal family. In 1878, Maharaj Shakti Singh of Bagore further extended the haveli by building the triple-arched gateway, and since then, it has been known as Bagore ki Haveli. Till 1947, the haveli remained in the possession of Mewar state. Post-India’s Independence, the Rajasthan government used the Haveli to house government employees. Find more information here: udaipurtourism.co.in/bagore-ki-haveli-udaipur </p>
-
-    <p>Haveli: <i>For non-Indians, the entry fee into the museum is ₹60(rupees) for adults and ₹30 (rupees) for children 5-12 years old. For Indian citizens and OCI Card holders, the entry fee into the museum is ₹100 (rupees) for adults and ₹50 (rupees) for children 5-12 years old. There is an additional charge of ₹50 (rupees) for each camera.</i></p>
-
-    <p>Dharodar Dance Show: <i>For non-Indians, the show is ₹150(rupees) for adults and ₹75 (rupees) for children 5-12 years old. For Indian citizens and OCI Card holders, the entry fee into the museum is ₹90 (rupees) for adults and ₹45 (rupees) for children 5-12 years old. There is an additional charge of ₹150 (rupees) for each camera.</i></p>
-
-    <h3>Shilpgram</h3>
-    📍Shilpgram, Udaipur, Bari, Rajasthan 313001, India
-
-    Shilpgram is a vibrant ethnographic museum and traditional crafts village spread over 70 acres. Established to promote and preserve the art, craft, and cultural heritage of rural India, particularly the Western states, it serves as a living museum depicting the lifestyles of the folk and tribal people of the region. The complex includes huts and structures representing different geographical and ethnic groups, providing insights into their traditional architecture, arts, and crafts. Shilpgram hosts an annual crafts fair at the end of December, attracting artisans and performers from across India, making it a hub for cultural exchange and a celebration of Indias rich rural arts and crafts.Find more information here: www.shilpgram.in/Home/Shilpgram-Udaipur
-    <p><i> Entry fee is ₹50 (rupees) for non-Indians and ₹30 (rupees) for Indian citizens and OCI card holders.</i></p>
-
-
-    <h3>Vintage Car Museum at Fateh Vilas by Fateh Collection</h3>
-
-    📍Bhujra, Sisarma Road, Old City, Brahmpuri, Udaipur, Rajasthan 313001, India
-
-    A must-visit for car lovers! The Vintage and Classic Car Museum is a car museum with some of the rarest classic and vintage car collections. This museum is owned by Arvind Singh Mewar, the king of Mewar (where Udaipur is). The museum also houses some famous cars, including the 1934 Rolls Royce used by James Bond in Octopussy! Find more information here: www.hrhhotels.com/vintage-classic-car-collection.php
-
-    <p><i> Entry fee into the museum is ₹400 (rupees) for adults and ₹250 (rupees) for children.</i></p>
-</div>
+    </CardContent>
+  </Card>
 </template>
 
 <script setup lang="ts">
